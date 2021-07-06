7cfcc7d7140854e39131db0b882ad7c00646556e
jangseohyun <plmaq11@gmail.com>
2021-07-06 23:47:18 +0900

    Spring MVC 실습 15차

---
+++ FileSystemApp01/src/com/test/mvc/SampleController.java
@@ -0,0 +1,30 @@
+/*======================================
+   SampleController.java
+   - 사용자 정의 컨트롤러 클래스
+======================================*/
+
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class SampleController implements Controller
+{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		// 액션 코드 
+		ModelAndView mav = new ModelAndView();
+		
+		
+		
+		return mav;
+
+	}
+	
+}
---
+++ FileSystemApp02/WebContent/Test1.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+	request.setCharacterEncoding("UTF-8");
+	String cp = request.getContextPath();
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Test1.jsp</title>
+</head>
+<body>
+
+	<!-- 『enctype="multipart/form-data"』: 파일 업로드를 하기 위한 필수 속성 -->
+	<!-- → 바이너리 형태로 전달 -->
+	<form action="Test3.jsp" method="post" enctype="multipart/form-data">
+		이름: <input type="text" name="name"><br>
+		파일: <input type="file" name="upload"><br>
+		<input type="submit" value="전송">
+	</form>
+
+</body>
+</html>
